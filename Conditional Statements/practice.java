c6e97e78646d70c245709997625f01b81856ad4e
VishalH01 <vishal.s.haramkar@gmail.com>
2025-07-05 16:25:24 +0530

    Conditional Statements part complete

---
+++ Conditional Statements/practice.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+public class practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // check if a number is even or odd
+        // System.out.println("Enter the number: ");
+        // int n = sc.nextInt();
+        // if((n%2 == 0)){
+        //     System.out.println("The number is even.");
+        // } else {
+        //     System.out.println("The number is odd.");
+        // }
+
+        // check if a number is positive, negative or zero
+        // System.out.println("Enter the number: ");
+        // int n = sc.nextInt();
+        // if(n > 0){
+        //     System.out.println("The number is positive.");
+        // } else if( n < 0) {
+        //     System.out.println("The number is negative.");
+        // } else {
+        //     System.out.println("The number is zero.");
+        // }
+
+        // check if a year is leap year or not
+        // System.out.println("Enter the year: ");
+        // int year = sc.nextInt();
+        // if(year%4 == 0 && year%100 != 0 || year%400 == 0){
+        //     System.out.println("The year is leap year.");
+        // } else {
+        //     System.out.println("The year is not leap year.");
+        // }
+
+        // income tax calculator
+        // System.out.println("Enter the income: ");
+        // int income = sc.nextInt();
+        // if(income < 500000){
+        //     System.out.println("No tax.");
+        // } else if (income >= 500000 && income <= 1000000){
+        //     System.out.println("The tax you have to pay based on your income : " + (int)(income * 0.18));
+        // } else {
+        //     System.out.println("The tax you have to pay based on your income : " + (int)(income * 0.30));
+        // }
+
+        //ternary operator example
+        // System.out.println("Enter the number: ");
+        // int n = sc.nextInt();
+        // System.out.println(n > 0 ? "The number is positive." : "The number is negative.");
+
+        // switch statement example
+        // System.out.println("Enter the number: ");
+        // int n = sc.nextInt();
+        // switch (n) {
+        //     case 1:
+        //         System.out.println("The number is one.");
+        //         break;
+        //     case 2:
+        //         System.out.println("The number is two.");
+        //         break;
+        //     case 3:
+        //         System.out.println("The number is three.");
+        //         break;
+        //     default:
+        //         System.out.println("The number is not one, two or three.");
+        //         break;
+        // }
+        
+        // switch statement for favorite fruit
+        // System.out.println("Enter the favorite fruit [apple, banana, orange]: ");
+        // String fruit = sc.next();
+        // switch (fruit) {
+        //     case "apple":
+        //         System.out.println("The favorite fruit is apple.");
+        //         break;
+        //     case "banana":
+        //         System.out.println("The favorite fruit is banana.");
+        //         break;
+        //     case "orange":
+        //         System.out.println("The favorite fruit is orange.");    
+        //         break;
+        //     default: 
+        //         System.out.println("The favorite fruit is not apple, banana or orange.");
+        //         break;
+        // }
+
+        
+    }
+}
